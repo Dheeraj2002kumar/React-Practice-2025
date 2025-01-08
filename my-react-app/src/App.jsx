e02e8a791bd36216eb4800e2050a81b0ba52d845
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-08 19:56:06 +0530

    Refactor App component to include Todos and manage state with hooks

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -12,31 +12,82 @@ import './App.css'
 // export default App
 
 
+// ---------------- Todos -----------------------------
+// Using memo will cause React to skip rendering a component if its props have not changed.
+
+// This can improve performance.
+
+// This section uses React Hooks. See the React Hooks section for more information on Hooks.
+
+// Problem
+// In this example, the Todos component re-renders even when the todos have not changed.
+
+
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import Todos from './Todos/Todos';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+export default App;
+
+
+
+
+// When you click the increment button, the Todos component re-renders.
+
+// If this component was complex, it could cause performance issues.
+
+
+
+
+
+//----------------------------------------------------------
+
+
+
 
 // --------------- React-Router ----------------
 
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="blogs" element={<Blogs />} />
+//           <Route path="contact" element={<Contact />} />
+//           <Route path="*" element={<NoPage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 
 
