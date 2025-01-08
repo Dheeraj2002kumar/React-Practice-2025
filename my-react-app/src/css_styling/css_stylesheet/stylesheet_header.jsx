34fd867697c2bc24662048ff3a1c7b80eba38fe3
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-08 20:36:59 +0530

    Add HeaderStylesheetEx component for improved styling in header

---
+++ my-react-app/src/css_styling/css_stylesheet/stylesheet_header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const HeaderStylesheetEx = () => {
+    return (
+        <>
+            <h1>Hello Style</h1>
+            <p>Add a little style!.</p>
+        </>
+    );
+}
+
+export default HeaderStylesheetEx;
