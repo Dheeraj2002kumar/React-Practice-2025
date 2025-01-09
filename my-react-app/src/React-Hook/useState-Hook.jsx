149c4db1f7b7dc27eada2b6665351fdfbf16be5e
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-09 20:41:14 +0530

    Refactor FavoriteColor component into Car component with enhanced state management using an object

--- my-react-app/src/React-Hook/useState-Hook.jsx
+++ my-react-app/src/React-Hook/useState-Hook.jsx
@@ -84,17 +84,158 @@ Use a button to update the state:
 
 */
 
+// import { useState } from "react";
+
+// function FavoriteColor (){
+//     const [color, setColor] = useState("red");
+
+//     return (
+//         <>
+//             <h1>My favorite color is {color}!</h1>
+//             <button type="button" onClick={() => setColor("blue")}>Blue</button>
+//         </>
+//     );
+// }
+
+// export default FavoriteColor;
+
+
+
+
+
+
+// -------------------------------------------------------
+
+/*
+
+What Can State Hold
+The useState Hook can be used to keep track of strings, numbers, booleans, arrays, objects, and any combination of these!
+
+We could create multiple state Hooks to track individual values.
+
+Example:
+Create multiple state Hooks:
+
+*/
+
+// import { useState } from "react";
+
+// function Car() {
+//     const [brand, setBrand] = useState("Ford")
+//     const [model, setModel] = useState("Mustang");
+//     const [year, setYear] = useState("1964");
+//     const [color, setColor] = useState("red");
+
+//     return (
+//         <>
+//             <h1>My {brand}</h1>
+//             <p>It is a {color} {model} from {year}.</p>
+           
+//             <button type="button" onClick={() => setColor("blue")}>Chang car detail</button>
+//         </>
+//     );
+
+// }
+
+// export default Car;
+
+
+
+
+
+
+
+// --------------------------------------------
+
+/*
+
+Or, we can just use one state and include an object instead!
+
+Example:
+Create a single Hook that holds an object:
+
+*/
+
+// import { useState } from "react";
+
+// function Car(){
+//     const [car, setCar] = useState({
+//         brand: "Ford",
+//         model: "Mustang",
+//         year: "1964",
+//         color: "red"
+//     });
+
+//     return (
+//         <>
+//             <h1>My {car.brand}</h1>
+//             <p>It is a {car.color} {car.model} from {car.year}.</p>
+//         </>
+//     );
+// }
+
+// export default Car;
+
+
+
+
+
+
+
+//-----------------------------------------------------
+
+/*
+
+Since we are now tracking a single object, we need to reference that object and then the property of that object when rendering the component. (Ex: car.brand)
+
+Updating Objects and Arrays in State
+When state is updated, the entire state gets overwritten.
+
+What if we only want to update the color of our car?
+
+If we only called setCar({color: "blue"}), this would remove the brand, model, and year from our state.
+
+We can use the JavaScript spread operator to help us.
+
+Example:
+Use the JavaScript spread operator to update only the color of the car: 
+
+*/
+
 import { useState } from "react";
 
-function FavoriteColor (){
-    const [color, setColor] = useState("red");
+function Car(){
+    const [car, setCar] = useState({
+        brand: "Ford",
+        model: "Mustang",
+        year: "1964",
+        color: "red"
+    });
+
+    const updateColor = () => {
+        setCar(previousState => {
+            return {...previousState, color: "blue" }
+        });
+    }
 
     return (
         <>
-            <h1>My favorite color is {color}!</h1>
-            <button type="button" onClick={() => setColor("blue")}>Blue</button>
+            <h1>My {car.brand}</h1>
+            <p>It is a {car.color} {car.model} from {car.year}.</p>
+
+            <button type="button" onClick={updateColor}>Blue</button>
         </>
     );
 }
 
-export default FavoriteColor;
+export default Car;
+
+
+
+/*
+
+Because we need the current value of state, we pass a function into our setCar function. This function receives the previous value.
+
+We then return an object, spreading the previousState and overwriting only the color.
+
+*/
