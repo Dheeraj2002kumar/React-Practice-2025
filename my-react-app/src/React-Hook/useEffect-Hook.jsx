3e77473acbbdf61b5e918e47e3689a56d1e645fc
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-16 13:31:53 +0530

    Fix Timer component to run useEffect only on initial render by adding an empty dependency array

--- my-react-app/src/React-Hook/useEffect-Hook.jsx
+++ my-react-app/src/React-Hook/useEffect-Hook.jsx
@@ -19,19 +19,80 @@ Use setTimeout() to count 1 second after initial render:
 */
 
 
+// import { useState, useEffect } from "react";
+// import ReactDOM from "react-dom";
+
+// function Timer(){
+//     const [count, setCount] = useState(0);
+
+//     useEffect(() => {
+//         setTimeout(() => {
+//             setCount((count) => count + 1);
+//         }, 1000);
+//     });
+
+//     return <h1>I've rendered {count} times!</h1>
+// }
+
+// export default Timer;
+
+
+
+/*
+
+But wait!! It keeps counting even though it should only count once!
+
+useEffect runs on every render. That means that when the count changes, a render happens, which then triggers another effect.
+
+This is not what we want. There are several ways to control when side effects run.
+
+We should always include the second parameter which accepts an array. We can optionally pass dependencies to useEffect in this array.
+
+Example
+1. No dependency passed:
+
+useEffect(() => {
+  //Runs on every render
+});
+
+
+Example
+2. An empty array:
+
+useEffect(() => {
+  //Runs only on the first render
+}, []);
+
+
+Example
+3. Props or state values:
+
+useEffect(() => {
+  //Runs on the first render
+  //And any time any dependency value changes
+}, [prop, state]);
+
+
+So, to fix this issue, let's only run this effect on the initial render.
+
+Example:
+Only run the effect on the initial render:
+
+*/
+
 import { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-function Timer(){
-    const [count, setCount] = useState(0);
+function Timer() {
+  const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setCount((count) => count + 1);
-        }, 1000);
-    });
+  useEffect(() => {
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+  }, []); // <- add empty brackets here
 
-    return <h1>I've rendered {count} times!</h1>
+  return <h1>I've rendered {count} times!</h1>;
 }
 
 export default Timer;
