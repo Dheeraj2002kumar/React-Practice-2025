42f4264f9cfbf3020ceb8235636d9eeb4687531e
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-17 09:55:11 +0530

    Implement UserContext to manage state across nested components

--- my-react-app/src/React-Hook/useContext-Hook.jsx
+++ my-react-app/src/React-Hook/useContext-Hook.jsx
@@ -20,48 +20,113 @@ Passing "props" through nested components:
 
 */
 
-import { useState } from "react";
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function Component1() {
+//   const [user, setUser] = useState("Jesse Hall");
+
+//   return (
+//     <>
+//       <h1>{`Hello ${user}!`}</h1>
+//       <Component2 user={user} />
+//     </>
+//   );
+// }
+
+// function Component2({ user }) {
+//   return (
+//     <>
+//       <h1>Component 2</h1>
+//       <Component3 user={user} />
+//     </>
+//   );
+// }
+
+// function Component3({ user }) {
+//   return (
+//     <>
+//       <h1>Component 3</h1>
+//       <Component4 user={user} />
+//     </>
+//   );
+// }
+
+// function Component4({ user }) {
+//   return (
+//     <>
+//       <h1>Component 4</h1>
+//       <Component5 user={user} />
+//     </>
+//   );
+// }
+
+// function Component5({ user }) {
+//   return (
+//     <>
+//       <h1>Component 5</h1>
+//       <h2>{`Hello ${user} again!`}</h2>
+//     </>
+//   );
+// }
+
+// export {Component1};
+
+// Even though components 2-4 did not need the state, they had to pass the state along so that it could reach component 5.
+
+
+
+/*
+
+The Solution
+The solution is to create context.
+
+Create Context
+To create context, you must Import createContext and initialize it:
+
+
+import { useState, createContext } from "react";
 import ReactDOM from "react-dom/client";
+
+const UserContext = createContext()
+
+
+Next we'll use the Context Provider to wrap the tree of components that need the state Context.
+
+Context Provider
+Wrap child components in the Context Provider and supply the state value.
+
 
 function Component1() {
   const [user, setUser] = useState("Jesse Hall");
 
   return (
-    <>
+    <UserContext.Provider value={user}>
       <h1>{`Hello ${user}!`}</h1>
       <Component2 user={user} />
-    </>
+    </UserContext.Provider>
   );
 }
 
-function Component2({ user }) {
-  return (
-    <>
-      <h1>Component 2</h1>
-      <Component3 user={user} />
-    </>
-  );
-}
 
-function Component3({ user }) {
-  return (
-    <>
-      <h1>Component 3</h1>
-      <Component4 user={user} />
-    </>
-  );
-}
+Now, all components in this tree will have access to the user Context.
 
-function Component4({ user }) {
-  return (
-    <>
-      <h1>Component 4</h1>
-      <Component5 user={user} />
-    </>
-  );
-}
+Use the useContext Hook
+In order to use the Context in a child component, we need to access it using the useContext Hook.
 
-function Component5({ user }) {
+First, include the useContext in the import statement:
+
+
+--------
+import { useState, createContext, useContext } from "react";
+--------
+
+
+Then you can access the user Context in all components:
+
+function Component5() {
+  const user = useContext(UserContext);
+
   return (
     <>
       <h1>Component 5</h1>
@@ -70,6 +135,67 @@ function Component5({ user }) {
   );
 }
 
-export {Component1};
 
-// Even though components 2-4 did not need the state, they had to pass the state along so that it could reach component 5.
+
+Full Example
+Example:
+Here is the full example using React Context:
+
+*/
+
+
+import { useState, createContext, useContext } from "react";
+import ReactDOM from "react-dom";
+
+const UserContext = createContext();
+
+function Component1(){
+    const [user, setUser] = useState("Jesse Hall");
+
+    return (
+        <UserContext.Provider value={user}>
+            <h1>{`Hello ${user}!`}</h1>
+            <Component2 />
+        </UserContext.Provider>
+    );
+}
+
+function Component2(){
+    return (
+        <>
+            <h1>Component 2</h1>
+            <Component3 />
+        </>
+    );
+}
+
+function Component3(){
+    return (
+        <>
+            <h1>Component 3</h1>
+            <Component4 />
+        </>
+    );
+}
+
+function Component4(){
+    return (
+        <>
+            <h1>Component 4</h1>
+            <Component5 />
+        </>
+    );
+}
+
+function Component5(){
+    const user = useContext(UserContext);
+
+    return (
+        <>
+            <h1>Component 5</h1>
+            <h2>{`Hello ${user} again!`}</h2>
+        </>
+    );
+}
+
+export {Component1};
