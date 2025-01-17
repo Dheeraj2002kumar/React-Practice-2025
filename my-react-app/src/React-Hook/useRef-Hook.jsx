1f4bff962857623da44a2bf7f2b28a6bf5f05f24
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-17 10:25:06 +0530

    Refactor App1 component to demonstrate useRef for focusing input element

--- my-react-app/src/React-Hook/useRef-Hook.jsx
+++ my-react-app/src/React-Hook/useRef-Hook.jsx
@@ -18,26 +18,75 @@ Use useRef to track application renders.
 
 */
 
-import { useState, useEffect, useRef } from "react";
+// import { useState, useEffect, useRef } from "react";
+// import ReactDOM from "react-dom";
+
+// function App1(){
+//     const [inputValue, setInputValue] = useState("");
+//     const count = useRef(0);
+
+//     useEffect(() => {
+//         // count.current = count.current + 1;
+//         count.current++;
+//     });
+
+//     return (
+//         <>
+//             <input 
+//             type="text"
+//             value={inputValue}
+//             onChange={(e) => setInputValue(e.target.value)}
+//             />
+//             <h1>Render Count: {count.current}</h1>
+//         </>
+//     );
+// }
+
+// export {App1};
+
+
+/* 
+
+useRef() only returns one item. It returns an Object called current.
+
+When we initialize useRef we set the initial value: useRef(0).
+
+It's like doing this: const count = {current: 0}. We can access the count by using count.current.
+
+Run this on your computer and try typing in the input to see the application render count increase.
+
+*/
+
+
+
+/*
+
+Accessing DOM Elements
+In general, we want to let React handle all DOM manipulation.
+
+But there are some instances where useRef can be used without causing issues.
+
+In React, we can add a ref attribute to an element to access it directly in the DOM.
+
+Example:
+Use useRef to focus the input:
+
+*/
+
+import { useRef } from "react";
 import ReactDOM from "react-dom";
 
 function App1(){
-    const [inputValue, setInputValue] = useState("");
-    const count = useRef(0);
+    const inputElement = useRef();
 
-    useEffect(() => {
-        // count.current = count.current + 1;
-        count.current++;
-    });
+    const focusInput = () => {
+        inputElement.current.focus();
+    };
 
     return (
         <>
-            <input 
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            />
-            <h1>Render Count: {count.current}</h1>
+            <input type="text" ref={inputElement} />
+            <button onClick={focusInput}>Focus Input</button>
         </>
     );
 }
