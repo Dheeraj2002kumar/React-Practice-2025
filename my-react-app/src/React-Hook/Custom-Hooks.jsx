1944793b6934ce0d35cf366fca1724bbdcec30a3
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-21 20:10:55 +0530

    Add documentation for creating custom React Hooks

---
+++ my-react-app/src/React-Hook/Custom-Hooks.jsx
@@ -0,0 +1,20 @@
+/*
+
+------------------------- React Custom Hooks --------------------------
+
+Hooks are reusable functions.
+
+When you have component logic that needs to be used by multiple components, we can extract that logic to a custom Hook.
+
+Custom Hooks start with "use". Example: useFetch.
+
+Build a Hook
+In the following code, we are fetching data in our Home component and displaying it.
+
+We will use the JSONPlaceholder service to fetch fake data. This service is great for testing applications when there is no existing data.
+
+To learn more, check out the JavaScript Fetch API section.
+
+Use the JSONPlaceholder service to fetch fake "todo" items and display the titles on the page:
+
+*/
